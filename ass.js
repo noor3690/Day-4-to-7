b1df6b68fcc2541f5f71cf4da2e519d803d836fe
noor3690 <164336345+noor3690@users.noreply.github.com>
2024-03-21 04:59:44 -0600

    Day 4
    100 days of coding challenge

---
+++ ass.js
@@ -0,0 +1,17 @@
+// Question 10
+// This is Noor ul ain
+// This program prints a personal message
+var Bestfriend = "Fariha api";
+var message = "Friendship is a sheltering tree";
+console.log("".concat(Bestfriend, "! ").concat(message));
+// Question 11
+var names = ['Maham', 'Maliha', 'Fariha api'];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+// Question 12
+var Friendnames = ['Maham', 'Maliha', 'Fariha api'];
+for (var _i = 0, Friendnames_1 = Friendnames; _i < Friendnames_1.length; _i++) {
+    var name_1 = Friendnames_1[_i];
+    console.log("Hellow ".concat(Friendnames, ", would you like to go with me"));
+}
---
+++ ass.ts
@@ -0,0 +1,18 @@
+// Question 10
+// This is Noor ul ain
+// This program prints a personal message
+let Bestfriend: string = "Fariha api";
+let message: string = "Friendship is a sheltering tree";
+console.log(`${Bestfriend}! ${message}`)
+
+// Question 11
+let names: string[] = ['Maham' , 'Maliha', 'Fariha api']
+for (let i = 0; i <names.length; i++) {
+    console.log(names[i]);
+}
+
+// Question 12
+let Friendnames: string[] = ['Maham','Maliha','Fariha api'];
+Friendnames.forEach(Friendnames => {
+    console.log(`Hellow ${Friendnames}, would you like to go with me`);
+});
